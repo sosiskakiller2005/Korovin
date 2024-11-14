5ca72d279d70804b552c27d073b024e58a1bbdff
Влад Козлов <vkozlov15012005@gmail.com>
2024-11-14 19:42:18 +0300

    Добавьте файлы проекта.

---
+++ Korovin/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Korovin.Model;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Korovin
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private static KorovinDbEntities _context;
+        public static KorovinDbEntities GetContext()
+        {
+            if (_context == null)
+            {
+                _context = new KorovinDbEntities();
+            }
+            return _context;
+        }
+    }
+}
